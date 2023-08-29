2601b1e0cb7dd9851904cf4b768a68ae078611f0
Usuario <Usuario@DESKTOP-7BNGDFF>
2023-08-28 20:33:16 -0500

    Subo el código que resuelve el ejercicio propuesto.

---
+++ src/main/java/com/mycompany/pootallerej2/POOtallerej2.java
@@ -0,0 +1,20 @@
+
+package com.mycompany.pootallerej2;
+//Esta es una prueba de escritorio para el pseudo-código propuesto en el seguimiento
+public class POOtallerej2 {
+
+    public static void main(String[] args) {
+        float x,y,suma;
+        suma= 0;
+        x= 20;
+        suma+=x;
+        y= 40;
+        x+=(Math.pow(y, 2));
+        suma+=(x/y);
+        System.out.println("El valor de la suma es: " + suma);
+                
+    }
+}
+
+
+
